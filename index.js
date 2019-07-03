a7cc22de32d43246976299b15117a71f6bb1c79e
Matt.Lin <matt.lin@87.office.6F.oa.vgs.tw>
2019-07-03 11:47:32 +0800

    modify the UI

--- index.js
+++ index.js
@@ -23,18 +23,39 @@ function createCard(data) {
   return card;
 }
 
+function createCards(data) {
+  var card = $("<div>")
+    .addClass("thumbnail our-team");
+  $("<img>")
+    .attr("src", data.image)
+    .attr("alt", "test")
+    .appendTo(card);
+  var content = $("<div>")
+    .addClass("team-content")
+    .appendTo(card);
+  $("<h3>")
+    .addClass("title")
+    .text(data.name)
+    .appendTo(content);
+  $("<span>")  
+    .addClass("post")
+    .text(TAGS[data.tags])
+    .appendTo(content);
+  return card;
+}
+
 function initList(data) {
   var list = $("#teamList");
   var row = null;
   $(data).each(function(index, team) {
-    if (index % 4 === 0) {
+    if (index % 3 === 0) {
       list.append(row);
-      row = $("<div>").addClass("row");
+      row = $("<div>").addClass("row form-group");
     }
     var col = $("<div>")
-      .addClass("col-6 col-sm-6 col-md-3 mb-4")
+      .addClass("col-md-4 col-sm-6")
       .appendTo(row);
-    col.append(createCard(team));
+    col.append(createCards(team));
   });
   list.append(row);
 }
@@ -43,29 +64,4 @@ $(document).ready(function() {
   if (TEAMS.length > 0) {
     initList(TEAMS);
   }
-  $("#teamModal").on("show.bs.modal", function(event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var teamIndex = button.data("index"); // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var team = TEAMS[teamIndex];
-    var modal = $(this);
-    modal.find(".modal-title").text(team.name);
-    modal
-      .find("img")
-      .attr("src", team.image)
-      .attr("alt", team.name);
-    var list = modal.find("#modal-detail");
-    $("<li>")
-      .text(team.name)
-      .appendTo(list);
-    $("<li>")
-      .text(team.intro)
-      .appendTo(list);
-  });
-
-  $("#teamModal").on("hide.bs.modal", function(event) {
-    var modal = $(this);
-    modal.find("#modal-detail").empty();
-  });
 });
