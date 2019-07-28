dd414fe43304628fa63715584d6c8312d4086fec
mattlin4567 <fought123@gmail.com>
2019-07-28 14:14:50 +0800

    Update teamsData.js
    0728

--- assets/js/teamsData.js
+++ assets/js/teamsData.js
@@ -4,30 +4,86 @@
 const TEAMS = [
   {
     index: 0,
-    name: "先行智庫股份有限公司",
+    name: "翎生科技有限公司",
     image: "./assets/images/20170526044625fbv.jpg",
+    intro:
+      "Blue Magpie Tech Corp.(BMTC)結合來自各領域的專家，致力於提供一站式的量身顧問服務；將AI及區塊鏈等技術引進金融、資本及新創產業，我們的使命是希望能夠成為推動產業升級以及協助企業掌握科技革命浪潮積極轉型、提升競爭優勢，共創雙贏！負責人黃芝翎在投資銀行業務和全球企業銀行業務方面擁有豐富的經驗，包括花旗銀行(於此期間認識鄒昌翰)，瑞士銀行和多家英國境內的銀行及歐盟和亞洲國有銀行的任職經驗，除此之外，她還曾在英國辦事處擔任部門主管，負責指導科學和創新項目；現在致力於區塊鏈領域，包括為金融機構，企業科技化以及協助新創公司和中小型企業從ICO及資本市場籌資。牛津大學理學學士和碩士。執行長鄒昌翰除了任職於花旗銀行和KPMG 顧問公司，其在金融服務和金融科技領域擁有相當廣泛的知識。他擅長於在新科技被大量運用前即發現市場趨勢，並建立戰略讓公司順應潮流和穩健發展。自2017年初以來，他一直致力於區塊鏈應用的開發，管理BlockEx亞洲辦事處，以及各種專案執行，並且首次代幣眾籌募集就超過2000萬美元。倫敦帝國理工學院的理學學士和碩士。",
+    tags: [0],
+  },
+  {
+    index: 1,
+    name: "AchievOR",
+    image: "./assets/images/se_20171220055217aqr.jpg",
+    intro:
+      "AchievOR 為台灣第一個以「技藝、經驗及興趣為主軸」串接起的職人社群，在這裡，每個人都可以專心做自己喜歡的事，並追尋自我滿足需求、成為一個Achievor。對我們來說，人生有很多可能，不該受到科系、職業、社會所框架住 ！讓職人能夠找到發揮的舞台、實現技能更大的專業價值是我們的使命，讓更多人能感受到這份熱情、看見每個不同的專業是我們對自己還有未來的期待！透過分享的課程與活動，更讓不論是上班族、親子、學生、團體等都能夠更深入、便捷探索與體驗各領域有趣人事物。並從職人對生活和技藝的熱情，感受到人生更多的可能性、創造對職業更不一樣的想像。AchievOR=Achieve+OR  你專心於喜歡的事，我致力於提供可能。而目前我們主要的團隊成員有兩位，每個人在自己負責的領域都有相對的經驗及專業。像是有負責對外關係管理的Vani，曾負責過許多商業行銷案、政府專案等。另一位為負責團隊內部管理的Ines，過去擔任過各式活動的美術總監、設計，在策略管理方面也有相當的經歷。除了我們之外，還有20幾位的職人共同與我們努力。期望能讓每個人都能成為AchievOR，也為更多人創造OR的機會。",
+    tags: [0],
+  },
+  {
+    index: 2,
+    name: "市民永續股份有限公司",
+    image: "./assets/images/se_20171006121210efu.jpg",
+    intro:
+      "市民永續股份有限公司是一間以區塊鏈技術，提供企業執行企業社會責任(CSR)措施之數據全球帳本服務，採用聯盟鏈架構，其中包含會計師數據確信簽章、金融單位社會責任投資(SRI)查看等多邊服務模式，發揮CSR策略效益，讓利害關係人獲得正面行動，企業經營者可從財務面上達成CSR永續經營效果。市民永續提供應用系統、雲端Baas、雲端開放API、認證數位簽帳、CSR內容揭露、CSR即時檢核等服務，讓企業、利害關係人、認證單位和金融服務單位能扮演適切角色，讓企業CSR措施能在財務策略上永續經營。為達成聯合國永續發展指標，發揮社會價值，我們不僅能協助大型企業，更重要的是透過區塊鏈建立量化程序和降低成本，協助中小企業遵循趨勢，實踐CSR，擴大民眾參與，達成全共同經營的願景。",
+    tags: [0],
+  },
+  {
+    index: 3,
+    name: "永訊智庫股份有限公司",
+    image: "./assets/images/se_20171013122322srj.jpg",
+    intro:
+      "Sustaihub永訊智庫為提供SaaS（軟體即服務）雲端服務的新創公司，運用Ai技術進行數據分析及應用，協助企業更有效率的導入永續發展的管理機制。團隊的核心能力在於AI技術與CSR顧問服務，橫跨兩個專業領域提供創新的解決方案。「永續資料搜尋引擎」匯整所有永續發展資料至大數據資料庫，提供搜尋引擎、分析工具、潛在風險評估等功能。透過網路爬蟲蒐集永續發展相關資料，包括CSR報告、獎項評比、產業訊息等。使用者可搜尋CSR報告（PDF格式）內文，搭配條件式搜尋功能，精準的找到對應的資料。在系統上也可建立視覺圖表進行產業分析，協助企業做出更永續的商業決策。「永續報告管理系統」以GRI國際框架為基礎，各部門可在雲端系統上盤點永續資料、協作編輯、進度管理、匯出報告等功能，更有效率的管理永續資料。數位化管理的效益，在於可搜尋過去的資料檔案、篩選分析特定的指標欄位、複製前一年報告內容，並直接轉換不同的國際標準，從GRI對照SDGs、CDP等國際框架，協助企業接軌國際永續趨勢。「數據分析顧問諮詢」則是整合上述的搜尋引擎及管理系統，藉以取得內外部的永續資料，透過數據分析完整掌握企業發展CSR的優劣勢。同時，透過推薦系統媒合永續理念相同的非營利組織，將CSR資源投入在需要的對象，促進企業與外部利害關係人的溝通合作。",
+    tags: [0],
+  },
+  {
+    index: 4,
+    name: "阿賴耶電影有限公司",
+    image: "./assets/images/se_20171013122322srj.jpg",
+    intro:
+      "「阿賴耶」(Alaya)在梵語中是積藏的意思，「阿賴耶識」即業(karma)種子的集聚和貯藏之處，是推動大千萬物運轉的一切因果之起源，也是真如本性的「本體基源」(ontological matrix)。這個概念貫徹了創辦人李奎穆對人類傳遞「故事」的本性及電影創作的影像哲學，也驅使著我們持續深耕影像創作及拓展影像新科技，並積極和其他領域跨界合作與激盪。阿賴耶電影有限公司自2018年底創立至今，已累積了外交部虛擬實境(VR)影片案《雙橡園》、輔導金長片《堀川》之3D VR前導短片《惡刑循環》、外交部TRA@40劇情短片《四十》…等作品，我們將秉持著人文關照的視角，凝視芸芸萬象，努力挖掘、探索聲音及影像語彙的全新疆界。",
+    tags: [0],
+  },
+  {
+    index: 5,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
     intro:
       "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
     tags: [0],
   },
   {
-    index: 1,
-    name: "好氏社會企業有限公司",
-    image: "./assets/images/se_20171220055217aqr.jpg",
+    index: 6,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
     intro:
       "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
-    tags: [1],
+    tags: [0],
   },
   {
-    index: 2,
-    name: "城田農業生技股份有限公司",
-    image: "./assets/images/se_20171006121210efu.jpg",
+    index: 7,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
     intro:
       "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
-    tags: [1],
+    tags: [0],
   },
   {
-    index: 3,
+    index: 8,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
+    intro:
+      "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
+    tags: [0],
+  },
+  {
+    index: 9,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
+    intro:
+      "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
+    tags: [0],
+  },
+  {
+    index: 10,
     name: "沃畝股份有限公司(元沛農坊)",
     image: "./assets/images/se_20171013122322srj.jpg",
     intro:
@@ -35,7 +91,7 @@ const TEAMS = [
     tags: [1],
   },
   {
-    index: 4,
+    index: 11,
     name: "沃畝股份有限公司(元沛農坊)",
     image: "./assets/images/se_20171013122322srj.jpg",
     intro:
@@ -43,7 +99,15 @@ const TEAMS = [
     tags: [1],
   },
   {
-    index: 5,
+    index: 12,
+    name: "沃畝股份有限公司(元沛農坊)",
+    image: "./assets/images/se_20171013122322srj.jpg",
+    intro:
+      "Lorem ipsum dolor sit amet, ad meis assum labitur has, qui delicata referrentur in. Bonorum conclusionemque ut mea, et aliquid inimicus adipiscing eam. Eum ea mollis dissentiunt. Impetus gubergren mea ea, pri id omnes propriae legendos. Id vel euismod atomorum, at assentior consectetuer vix. Id duo solum summo.",
+    tags: [1],
+  },
+  {
+    index: 13,
     name: "沃畝股份有限公司(元沛農坊)",
     image: "./assets/images/se_20171013122322srj.jpg",
     intro:
