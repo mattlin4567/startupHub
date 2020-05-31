b303c02a30f9971eceffa8ff68d00800a90d305d
Matt Lin <matt.lin@iask.today>
2020-05-31 22:54:39 +0800

    1. 調整 bootstap 版本 2. 新增團隊期別選單 3. 新增第二期團隊資料 4. 移除首頁影片

--- index.js
+++ index.js
@@ -1,109 +1,95 @@
-function createCards(data) {
-  var img = `./assets/images/${data.index}/logo.jpg`;
+function createCards(data, year) {
+  var img = `./assets/images/${year}/${data.index}/logo.jpg`;
   var card = $('<div>')
     .addClass('thumbnail our-team')
-    .attr('data-index', data.index);
-  $('<img>')
-    .attr('src', img)
-    .attr('alt', 'test')
-    .appendTo(card);
-  var content = $('<div>')
-    .addClass('team-content')
-    .appendTo(card);
-  $('<h3>')
-    .addClass('title')
-    .text(data.name)
-    .appendTo(content);
-  $('<span>')
-    .addClass('post')
-    .text(TAGS[data.tags])
-    .appendTo(content);
+    .attr('data-index', data.index)
+    .attr('data-year', year);
+  $('<img>').attr('src', img).attr('alt', 'test').appendTo(card);
+  var content = $('<div>').addClass('team-content').appendTo(card);
+  $('<h3>').addClass('title').text(data.name).appendTo(content);
+  $('<span>').addClass('post').text(TAGS[data.tags]).appendTo(content);
   return card;
 }
 
 function createSideMenu(data) {
-  var item = $('<div>')
-    .text(data.name)
-    .attr('data-index', data.index);
+  var item = $('<div>').text(data.name).attr('data-index', data.index);
   return item;
 }
 
-function createMobileList(data) {
-  var img = `./assets/images/${data.index}/logo.jpg`;
-  var item = $('<div>')
-    .addClass('list-item')
-    .attr('data-index', data.index);
+function createMobileList(data, year) {
+  var img = `./assets/images/${year}/${data.index}/logo.jpg`;
+  var item = $('<div>').addClass('list-item').attr('data-index', data.index);
   var avatar = $('<div>').addClass('list-item-avatar');
-  $('<img>')
-    .addClass('avatar')
-    .attr('src', img)
-    .appendTo(avatar);
-  var content = $('<div>')
-    .addClass('list-item-content')
-    .text(data.name);
+  $('<img>').addClass('avatar').attr('src', img).appendTo(avatar);
+  var content = $('<div>').addClass('list-item-content').text(data.name);
   var chevron = $('<div>').addClass('list-item-chevron');
-  $('<i>')
-    .addClass('fa fa-chevron-right')
-    .appendTo(chevron);
+  $('<i>').addClass('fa fa-chevron-right').appendTo(chevron);
   item.append(avatar);
   item.append(content);
   item.append(chevron);
   return item;
 }
 
-function initPage(data) {
+function initPage(years) {
   var list = $('#team-list');
   var sideMenu = $('.side-menu');
   var row = $('<div>').addClass('row form-group hidden-xs');
   var mobile = $('<div>').addClass('visible-xs-block');
-  $(data).each(function(index, team) {
-    var col = $('<div>')
-      .addClass('col-sm-3 col-xs-6')
-      .appendTo(row);
-    col.append(createCards(team));
-    sideMenu.append(createSideMenu(team));
-    mobile.append(createMobileList(team));
+  years.forEach((y) => {
+    var data = TEAMS[y];
+    $(data).each(function (index, team) {
+      var col = $('<div>').addClass('span3').appendTo(row);
+      col.append(createCards(team, y));
+      sideMenu.append(createSideMenu(team));
+      mobile.append(createMobileList(team, y));
+    });
   });
   list.append(row);
   list.append(mobile);
-}
-
-function navgation() {
-  window.location = './intro.html?team=' + $(this).attr('data-index');
-}
-
-function onYouTubeIframeAPIReady() {
-  var height = '695';
-  var width = '1140';
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    height = '201';
-    width = '345';
-  }
-  player = new YT.Player('intro-video', {
-    videoId: 'H6zsD_G9bt8',
-    height: height,
-    width: width
-  });
-}
-
-$(document).ready(function() {
-  if (TEAMS.length > 0) {
-    initPage(TEAMS);
-  }
-  $('.float').hover(
-    function() {
-      $(this)
-        .stop()
-        .animate({ right: '0' }, 'medium');
-    },
-    function() {
-      $(this)
-        .stop()
-        .animate({ right: '-20vw' }, 'medium');
-    },
-    500
-  );
+  $('.cv-spinner').hide();
+  $('#team-list').show();
+  // bind click event
   $('div.thumbnail').click(navgation);
   $('.side-menu div').click(navgation);
   $('.list-item').click(navgation);
+}
+
+function navgation() {
+  var team = $(this).attr('data-index');
+  var year = $(this).attr('data-year');
+  console.info(`${year}, ${team}`);
+  window.location = `./intro.html?team=${team}&year=${year}`;
+}
+
+$(document).ready(function () {
+  var TOTALYEAR = ['108', '109'];
+  var selectedTeam = ['109'];
+  if (selectedTeam.length > 0) {
+    initPage(selectedTeam);
+  }
+  $('.float').hover(
+    function () {
+      $(this).stop().animate({ right: '0' }, 'medium');
+    },
+    function () {
+      $(this).stop().animate({ right: '-20vw' }, 'medium');
+    },
+    500
+  );
+  $('.dropdown-menu>li>a').click(function () {
+    var list = $('#team-list');
+    var sideMenu = $('.side-menu');
+    $('.cv-spinner').show();
+    list.hide();
+    list.empty();
+    sideMenu.empty();
+    var selection = $(this).attr('data-value');
+    var select = $('#team-select');
+    select.html($(this).text()).append('<span class="caret"></span>');
+    selectedTeam = selection === 'all' ? TOTALYEAR : [selection];
+
+    setTimeout(() => {
+      initPage(selectedTeam);
+    }, 500);
+  });
 });
--- intro.js
+++ intro.js
@@ -4,10 +4,8 @@ function initIntro(team) {
   var description = $('.project_description');
   var buttonGroup = $('#social-link');
   $('#team_name h4 span').text(team.name);
-  team.intro.forEach(element => {
-    $('<p>')
-      .text(element)
-      .appendTo(description);
+  team.intro.forEach((element) => {
+    $('<p>').text(element).appendTo(description);
   });
   if (team.web) {
     buttonGroup.append(initSocialButton(0, team.web));
@@ -18,23 +16,19 @@ function initIntro(team) {
 }
 function initSocialButton(type, url) {
   var icon = type ? 'fa fa-facebook' : 'fa fa-globe';
-  var btn = $('<button>')
-    .attr('type', 'button')
-    .addClass('btn');
-  $('<i>')
-    .addClass(icon)
-    .appendTo(btn);
-  btn.bind('click', function() {
+  var btn = $('<button>').attr('type', 'button').addClass('btn');
+  $('<i>').addClass(icon).appendTo(btn);
+  btn.bind('click', function () {
     window.location = url;
   });
   return btn;
 }
-function initImagesCarousel(team) {
+function initImagesCarousel(team, year) {
   var list = $('.carousel-inner');
   var indicator = $('.carousel-indicators');
   var indicatorIndex = 0;
   var index = team.index;
-  
+
   if (team.youtube) {
     var item = $('<div>').addClass('item').appendTo(list);
     $('<div>').attr('id', 'player').appendTo(item);
@@ -48,12 +42,9 @@ function initImagesCarousel(team) {
     if (i === 1 && index == 8) {
       continue;
     }
-    var img = `./assets/images/${index}/carousel${i + 1}.jpg`;
+    var img = `./assets/images/${year}/${index}/carousel${i + 1}.jpg`;
     var item = $('<div>').addClass('item');
-    $('<img>')
-      .attr('src', img)
-      .css('width', '100%')
-      .appendTo(item);
+    $('<img>').attr('src', img).css('width', '100%').appendTo(item);
     list.append(item);
     $('<li>')
       .attr('data-target', '#myCarousel')
@@ -61,38 +52,22 @@ function initImagesCarousel(team) {
       .appendTo(indicator);
     indicatorIndex++;
   }
-  list
-    .children()
-    .first()
-    .addClass('active');
-  indicator
-    .children()
-    .first()
-    .addClass('active');
+  list.children().first().addClass('active');
+  indicator.children().first().addClass('active');
 }
 
-function initTeamsCarousel(data) {
+function initTeamsCarousel(data, year) {
   var list = $('#carousel_list');
-  $(data).each(function(index, team) {
-    var img = `./assets/images/${index}/logo.jpg`;
+  $(data).each(function (index, team) {
+    var img = `./assets/images/${year}/${index}/logo.jpg`;
     var card = $('<div>')
       .addClass('thumbnail our-team')
-      .attr('data-index', team.index);
-    $('<img>')
-      .attr('src', img)
-      .attr('alt', 'test')
-      .appendTo(card);
-    var content = $('<div>')
-      .addClass('team-content')
-      .appendTo(card);
-    $('<h3>')
-      .addClass('title')
-      .text(team.name)
-      .appendTo(content);
-    $('<span>')
-      .addClass('post')
-      .text(TAGS[team.tags])
-      .appendTo(content);
+      .attr('data-index', team.index)
+      .attr('data-year', year);
+    $('<img>').attr('src', img).attr('alt', 'test').appendTo(card);
+    var content = $('<div>').addClass('team-content').appendTo(card);
+    $('<h3>').addClass('title').text(team.name).appendTo(content);
+    $('<span>').addClass('post').text(TAGS[team.tags]).appendTo(content);
     list.append(card);
   });
 }
@@ -104,10 +79,10 @@ function getTeamIndex() {
 
 function onPlayerStateChange(event) {
   if (event.data === YT.PlayerState.PLAYING) {
-    $("#myCarousel").carousel("pause");
-  } 
-  if(event.data === YT.PlayerState.PAUSED){
-    $("#myCarousel").carousel("cycle");
+    $('#myCarousel').carousel('pause');
+  }
+  if (event.data === YT.PlayerState.PAUSED) {
+    $('#myCarousel').carousel('cycle');
   }
 }
 
@@ -119,20 +94,27 @@ function onYouTubeIframeAPIReady() {
     width = '345';
   }
   player = new YT.Player('player', {
-    videoId: TEAMS[getTeamIndex()].youtube,
+    videoId: TEAMS[getTeamYear()][getTeamIndex()].youtube,
     height: height,
     width: width,
     events: {
-      'onStateChange': onPlayerStateChange
-    }
+      onStateChange: onPlayerStateChange,
+    },
   });
 }
 
-$(document).ready(function() {
+function getTeamYear() {
+  var results = new RegExp('[?&]year=([^&#]*)').exec(window.location.search);
+  return results !== null ? results[1] || 0 : false;
+}
+
+$(document).ready(function () {
   var teamIndex = getTeamIndex();
-  initIntro(TEAMS[teamIndex]);
-  initImagesCarousel(TEAMS[teamIndex]);
-  initTeamsCarousel(TEAMS);
+  var teamYear = getTeamYear();
+  var data = TEAMS[teamYear];
+  initIntro(data[teamIndex]);
+  initImagesCarousel(data[teamIndex], teamYear);
+  initTeamsCarousel(data, teamYear);
   $('.owl-carousel').owlCarousel({
     startPosition: teamIndex,
     loop: true,
@@ -153,25 +135,34 @@ $(document).ready(function() {
     },
   });
 
-  $('div.thumbnail').click(function() {
-    window.location = './intro.html?team=' + $(this).attr('data-index');
+  $('div.thumbnail').click(function () {
+    var team = $(this).attr('data-index');
+    var year = $(this).attr('data-year');
+    window.location = `./intro.html?team=${team}&year=${year}`;
   });
 
-  $("#myCarousel").on('slide.bs.carousel', function (){
-    if($("#player").length){
+  $('#myCarousel').on('slide.bs.carousel', function () {
+    if ($('#player').length) {
       player.stopVideo();
     }
   });
 
-  $("#myCarousel").swipe( {
+  $('#myCarousel').swipe({
     //Generic swipe handler for all directions
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      if(direction==='left'){
-        $("#myCarousel").carousel("next");
+    swipe: function (
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction === 'left') {
+        $('#myCarousel').carousel('next');
       }
-      if(direction==='right'){
-        $("#myCarousel").carousel("prev")
+      if (direction === 'right') {
+        $('#myCarousel').carousel('prev');
       }
-    }
+    },
   });
 });
